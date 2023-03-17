704d40f6f9d3ab54326a4aebb97f9cb7075631f9
Shivam-tamboli <shivamtamboli63@gmail.com>
2023-03-17 19:39:42 +0530

    COMMIT NEW

---
+++ day3.py
@@ -0,0 +1,19 @@
+num1 = input("enter the first number:")
+num2 = input("enter the second number:")
+
+#add to numbers
+sum = float(num1) + float(num2)
+#subtract to numbers
+sub = float(num1) - float(num2)
+#multiplication of two numbers
+mul = float(num1) * float(num2)
+#divide to numbers 
+div = float(num1) / float(num2)
+#display the sum
+print(format,num1,num2,sum)
+#display the subtractoin
+print(format,num1,num2,sub)
+#display the division 
+print(format,num1,num2,div)
+#display the multiplication
+print(format,num1,num2,mul)
