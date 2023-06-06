4fec096593636b3327a64e7b4b89177253c28eda
Shivam-tamboli <shivamtamboli63@gmail.com>
2023-06-06 23:46:17 +0530

    printed half pyramid

---
+++ May/June/Pyramidpartten.py
@@ -0,0 +1,18 @@
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print("* ", end="")
+    print("\n")
+
+
+#Expected output
+# Enter number of rows: 5
+# *       
+# * * 
+
+# * * * 
+
+# * * * * 
+
+# * * * * *
