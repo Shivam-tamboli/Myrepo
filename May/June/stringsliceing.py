37d6370b6b0fe6eb7001b7be657227071908acae
Shivam-tamboli <shivamtamboli63@gmail.com>
2023-06-07 17:40:27 +0530

    string sliceing

---
+++ May/June/stringsliceing.py
@@ -0,0 +1,21 @@
+String = 'Astring'
+print(type(String))
+# <class 'str'>
+
+print(String[5])
+print(String[1])
+print(String[1:4])
+print(String[ : 4])
+print(String[:])
+print(String[-5:-1])
+print(String[:-1:])
+print(String[0:78])
+#Expected output
+#<class 'str'>
+#n
+#str
+#Astr
+#Astring
+#tring
+#Astrin
+#Astring
