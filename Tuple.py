2fcd6ba671e920ddbc8f95300a6d62fe08d28911
Shivam Tamboli <shivamtamboli66@gmail.com>
2023-06-10 23:58:21 +0530

    Using loop and conditions in tuple

---
+++ Tuple.py
@@ -0,0 +1,11 @@
+#create a tuple
+tuple = (1,2,3,4,5,6,76,78,9,9)  
+  
+# Initiating the loop  
+for value in tuple:  
+    if value % 2 != 0:  
+        print(value)  
+# giving an else statement  
+else:  
+    print("These are the odd numbers present in the tuple") 
+        
