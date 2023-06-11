b0805d2c26cebd9ff139f590f74d777fee9512bb
Shivam Tamboli <shivamtamboli66@gmail.com>
2023-06-11 23:06:44 +0530

    Sum of numbers in list

---
+++ loop.py
@@ -0,0 +1,11 @@
+#create a list.
+numbers = [12,3,45,35,5,32,54,78,8,65]
+print(type(numbers))
+#initialize a variale that will store the sum 
+sum = 0
+#using loop
+for num in numbers:
+    sum = sum + num**2
+#print the sum
+print(sum)
+
