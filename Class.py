96d1eee3bf8bcb2fcb1c217652beee941ef98a09
shivam Tamboli <shivamtamboli66@gmail.com>
2023-06-16 23:07:24 +0530

    Created a class and constructor and gave argunments

---
+++ Class.py
@@ -0,0 +1,15 @@
+# Created a class name of class is Person.
+class Person:
+#Created a constructor and gave argunment.
+    def __init__(self, name, age):
+        # The arguments.
+        self.name = name
+        self.age = age
+
+# Created a object of this class.
+Shivam = Person 
+# printed name and age.
+print("Satyam",87)
+
+# Expected output.
+# Satyam 87
