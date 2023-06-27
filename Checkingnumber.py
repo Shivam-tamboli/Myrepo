97e23b01e3fd07cd21ad541dd8a539d294c6246f
shivam Tamboli <shivamtamboli66@gmail.com>
2023-06-27 23:17:40 +0530

    Taking input from the user and checking the number it is a positive or negative

---
+++ Checkingnumber.py
@@ -0,0 +1,21 @@
+# Printed a statement.
+print("Enter the number for checking number is a not positive or negative:")
+# Taking input from the user.
+# Storeing in a num 
+num = (int(input()))
+# Using if condition.
+if num > 0:
+    print("It is a positve number.")
+# Using else condition
+elif num < 0:
+    print("It is a negative number.")
+
+
+# Expected output.
+# Enter the number for checking number is a not positive or negative:
+# 56
+# It is a positve number.
+ 
+# Enter the number for checking number is a not positive or negative:
+# -1
+# It is a negative number.
