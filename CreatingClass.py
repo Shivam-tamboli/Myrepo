a0fea903b7c7229b646cb108c251db8c6d2088b8
shivam Tamboli <shivamtamboli66@gmail.com>
2023-06-28 23:22:31 +0530

    Created a class name by student created two objects

---
+++ CreatingClass.py
@@ -0,0 +1,17 @@
+# created a class name by student.
+class student:
+    pass 
+# created two object of this class.
+shivam = student()
+satyam = student()
+
+# shivam's standard
+shivam.std = 12
+# shivam's age 
+shivam.age = 20
+# printing the age and standard of shivam's
+print(shivam.std,shivam.age)
+
+
+# Expected output.
+# 12 20
